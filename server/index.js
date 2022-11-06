3931dda6d89944de4f2afa247bd785680f85f5f3
Kevin <86487929+tar83@users.noreply.github.com>
2022-11-06 13:04:25 -0500

    exercise 8.6

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -89,7 +90,7 @@ const typeDefs = gql`
 
   type Author {
     name: String!
-    born: Int!
+    born: Int
     bookCount: Int
     id: ID!
   }
@@ -99,6 +100,15 @@ const typeDefs = gql`
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
   }
 `;
 
@@ -129,6 +139,19 @@ const resolvers = {
           bookCount: authorBooks.length,
         };
       });
+    },
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.includes(args.author)) {
+        const author = { name: args.author, id: uuid() };
+        authors = authors.concat(author);
+      }
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+
+      return book;
     },
   },
 };
