55130968be63a70dfc5f14668a59c0f67be211df
Kevin <86487929+tar83@users.noreply.github.com>
2022-11-09 15:48:25 -0500

    exercise 8.19

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -1,8 +1,11 @@
 import { ALL_BOOKS } from "../queries";
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 
 const Books = (props) => {
   const allBooksQuery = useQuery(ALL_BOOKS);
+  const [genreSelection, setGenreSelection] = useState("");
+
   if (!props.show) {
     return null;
   }
@@ -12,10 +15,37 @@ const Books = (props) => {
   }
 
   const books = allBooksQuery.data.allBooks;
+  const genres = books.map((book) => {
+    return book.genres;
+  });
+  const uniqueGenres = [...new Set(genres.join().split(","))];
+
+  const handleSelection = (genre) => {
+    setGenreSelection(genre);
+  };
+
+  const booksMap = (a) => (
+    <tr key={a.title}>
+      <td>{a.title}</td>
+      <td>{a.author.name}</td>
+      <td>{a.published}</td>
+    </tr>
+  );
 
   return (
     <div>
       <h2>Books</h2>
+
+      {uniqueGenres.map((g) => (
+        <button key={g} onClick={() => handleSelection(g)}>
+          {g}
+        </button>
+      ))}
+      <button onClick={() => handleSelection("all")}>all genres</button>
+
+      <div>
+        in genre <strong>{genreSelection}</strong>
+      </div>
 
       <table>
         <tbody>
@@ -24,13 +54,11 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {genreSelection === "all"
+            ? books.map(booksMap)
+            : books
+                .filter((a) => a.genres.includes(genreSelection))
+                .map(booksMap)}
         </tbody>
       </table>
     </div>
